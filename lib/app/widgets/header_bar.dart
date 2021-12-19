bad8fa8ac17b09d6fbb2d246e89f5f1bc79894a4
Erderis <deris.dev7@gmail.com>
2021-12-19 08:25:45 +0700

    bug fixes

--- lib/app/widgets/header_bar.dart
+++ lib/app/widgets/header_bar.dart
@@ -7,9 +7,13 @@ class HeaderBar extends StatelessWidget {
   const HeaderBar({
     Key? key,
     required this.title,
+    this.haveBackButton = true,
+    this.trailing,
   }) : super(key: key);
 
   final String title;
+  final bool haveBackButton;
+  final Widget? trailing;
 
   @override
   Widget build(BuildContext context) {
@@ -23,15 +27,17 @@ class HeaderBar extends StatelessWidget {
               ),
               fit: BoxFit.fill)),
       child: Padding(
-        padding: EdgeInsets.only(left: 22, top: 30),
+        padding: EdgeInsets.only(left: 22, top: 30, right: 22),
         child: Row(
           children: [
-            InkWell(
-                onTap: () => Get.back(),
-                child: Icon(
-                  Icons.arrow_back,
-                  color: Palette.white,
-                )),
+            !haveBackButton
+                ? SizedBox()
+                : InkWell(
+                    onTap: () => Get.back(),
+                    child: Icon(
+                      Icons.arrow_back,
+                      color: Palette.white,
+                    )),
             SizedBox(
               width: 10,
             ),
@@ -40,6 +46,8 @@ class HeaderBar extends StatelessWidget {
                     color: Palette.white,
                     fontSize: 17,
                     fontFamily: AppFontStyle.montserratMed)),
+            Spacer(),
+            trailing ?? SizedBox()
           ],
         ),
       ),
